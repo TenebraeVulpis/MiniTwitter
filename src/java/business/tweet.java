74dcf5312cf04b6baeaad3839f8481360c4dc8c0
NoDragonsPlz <nicolas.wilhoit@gmail.com>
2018-11-11 21:06:28 -0700

    Create tweet.java

---
+++ src/java/business/tweet.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+import java.io.Serializable;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat; 
+/**
+ *
+ * @javabean for Tweet Entity
+ */
+public class Tweet implements Serializable {
+    // define attributes
+    private String composerEmail;
+    private String message;
+    private boolean mentions;
+    private String date; 
+    
+    public Tweet() {
+        composerEmail = "";
+        message = "";
+        mentions = false;
+        date = "";
+    }
+    
+    //setters and getters
+    public String getcomposerEmail() {
+        return this.composerEmail;
+    }
+    public void setcomposerEmail(String composerEmail) {
+        this.composerEmail = composerEmail;
+    }
+    public String getMessage(){
+        return this.message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public boolean getMentions() {
+        return this.mentions;
+    }
+    public void setMentions(boolean mentions) {
+        this.mentions = mentions;
+    }
+    public void setdate(String date) {
+        this.date = date;
+    }
+    public String getdate() {
+        return this.date;
+    }
+    //public String getDate() {
+    //    Timestamp ts = this.date;
+    //    Date date = new Date();
+    //    date.setTime(ts.getTime());
+    //    String formattedDate = new SimpleDateFormat("yyyyMMdd hh:mm:ss").format(date);
+    //    
+    //    return formattedDate;
+    //} 
+}
